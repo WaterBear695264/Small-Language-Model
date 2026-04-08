a6e9a8cc28c3ced78ff9dab46aa3094503f0b481
WaterBear695264 <142440070+WaterBear695264@users.noreply.github.com>
2026-04-08 18:35:01 +0000

    Finished the text filtering system

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const response = await fetch('myfile.txt');
+const response = await fetch('testFile.txt');
 const text = await response.text();
-console.log(text)
+let newText = text.replace(/[{}();|<>~,!?@#$%^&*.:_—]/g, "");
+newText = newText.replace(/\n/g, " ")
+newText = newText.replace(/\s\s+/g, ' ');
+newText = newText.replace(/[A-Z]/g, (match) => {
+    return match.toLowerCase()
+})
+console.log("all I do is win", newText);
+
+
