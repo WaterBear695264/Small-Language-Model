5f57be31f5ead8db00f4bef96f23e7423e317b90
WaterBear695264 <142440070+WaterBear695264@users.noreply.github.com>
2026-04-08 18:51:37 +0000

    started frequency map

--- script.js
+++ script.js
@@ -6,6 +6,42 @@ newText = newText.replace(/\s\s+/g, ' ');
 newText = newText.replace(/[A-Z]/g, (match) => {
     return match.toLowerCase()
 })
-console.log("all I do is win", newText);
+let newArr = []
+
+for(let i = 0; i < newText.length; i++){
+    if(newText[i] === " "){
+        if(!newArr.includes(newText.substring(0, i))){
+            newArr.push(newText.substring(0, i))
+            newText = newText.substring(i+1)
+            i = 0
+        }else{
+            newText = newText.substring(i+1)
+            i = 0
+        }
+
+    }
+}
+
+
+
+let freqMap = {};
+
+const createMap = (starterObject) => {
+    for(let i = 0; i < newText.length; i++){
+        if(newText[i] === " "){
+            if(!newArr.includes(newText.substring(0, i))){
+                starterObject[newText.substring(0, i)] = {}
+                newText = newText.substring(i+1)
+                i = 0
+            }else{
+                newText = newText.substring(i+1)
+                i = 0
+            }
+
+    }
+}
+
+
+console.log("all I do is win", newText, newArr);
 
 
